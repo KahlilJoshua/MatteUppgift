a56852740d454bec57f88257ba10993c9933bebd
KahlilJoshua <136188721+KahlilJoshua@users.noreply.github.com>
2023-09-13 21:35:19 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,78 +1,52 @@
 <!DOCTYPE html>
-<html lang="sv">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MatteUppgift1</title>
-    <link rel="stylesheet" href="style.css">
+    <title>MatteSpel</title>
 </head>
+
 <body>
-    <h1>Matte uppgifter</h1>
-    <p>"+" för plus</p>
-    <p>"-" för minus</p>
-    <p>"*" för gånger</p>
-    <p>"/" för delat</p>
-    
-    <form action="index.php" method="post">
-        <label>tal 1:</label>
-        <input type="text" name="x">
-        <br>
-        <br>
-        <label>Tecken: </label>
-        <input type="text" name="t">
-        <br>
-        <br>
-        <label>tal 2:</label>
-        <input type="text" name="y">
-
-        <input type="submit">
+    <p>Vad är 1 x 2?</p>
+    <form action="index.php" method="get">
+        <input type="text" name="svar">
+        <input type="submit" name="sub">
     </form>
-    <h2>Se nedan för primtal check</h2>
-    <form action="index.php" method="post">
-        <label >Skriv ett tal för primtal check</label>
-        <input type="text" name="p">
-        <input type="submit">
-    </form>
-</body>
-</html>
+    <?php
 
-
-
-<?php
-$x = $_POST["x"];
-$t = $_POST["t"];
-$y = $_POST["y"];
-
-switch($t){
-    case "+":
-        echo $x + $y; 
-        break;
-    case "-":
-        echo $x - $y;
-        break;
-    case "*":
-        echo $x * $y;
-        break;
-    case "/": 
-        echo $x / $y;
-        break;
-
-}
-
-$flag = 0;
-$p = $_POST["p"];
-
-for($i = 1; $i<=$p; $i++){
-    if($p % $i == 0){
-        $flag ++;
+    $facit = array(2, 4, 5, 3, 8, 32);
+    if ($_GET["svar"] == $facit[0]) {
+        echo "Rätt";
     }
-    
-}
-if($flag == 2){
-    echo " ditt tal är ett primtal ";
-}
-else{
-    echo " ditt tal är inte ett primtal ";
-}
-?>
 
+    ?>
+    <p>Vad är 2 x 2?</p>
+    <form action="index.php" method="get">
+        <input type="text" name="svar">
+        <input type="submit" name="sub">
+    </form>
+    <?php
+
+    $facit = array(2, 4, 5, 3, 8, 32);
+    if ($_GET["svar"] == $facit[1]) {
+        echo "Rätt";
+    }
+
+    ?>
+    <p>Vad är 3 x 2?</p>
+    <form action="index.php" method="get">
+        <input type="text" name="svar">
+        <input type="submit" name="sub">
+    </form>
+    <?php
+
+    $facit = array(2, 4, 6, 3, 8, 32);
+    if ($_GET["svar"] == $facit[2]) {
+        echo "Rätt";
+    }
+    ?>
+
+</body>
+
+</html>
